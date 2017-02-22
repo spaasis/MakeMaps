af77b6390986ddf31e9b369109bd3340804d343b
Simo Paasisalo <simopaasisalo@fastmail.com>
2017-02-22 09:52:46 +0200

    Added selenium and test directory

---
+++ tests/test_loads.js
@@ -0,0 +1,9 @@
+
+var webdriver = require('selenium-webdriver'),
+    By = require('selenium-webdriver').By,
+    until = require('selenium-webdriver').until;
+
+
+var driver = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
